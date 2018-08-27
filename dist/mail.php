aa0719884a4d7732e5c9d5397bd69183a6400638
KononovD <KononovD@users.noreply.github.com>
2018-08-27 21:11:00 +0300

    5
    5

--- dist/mail.php
+++ dist/mail.php
@@ -44,23 +44,9 @@ function adopt($text) {
 	return '=?UTF-8?B?'.Base64_encode($text).'?=';
 }
 
-
-
 $headers = "MIME-Version: 1.0" . PHP_EOL .
 "Content-Type: text/html; charset=utf-8" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
 mail($admin_email, adopt($form_subject), $message, $headers );
-
-
-server {
-listen 80;
-server_name localhost;
-location / {
-root html;
-index index.html index.htm;
-}
-error_page 405 =200 $uri;
-# ...
-}
