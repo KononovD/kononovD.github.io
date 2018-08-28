9c6d01b89f15afc6f69fa9287902e117a58338cf
KononovD <KononovD@users.noreply.github.com>
2018-08-28 17:44:19 +0300

    1

---
+++ форма пхп/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <meta charset="utf-8"/>
+</head>
+
+<body>
+  <form action="/testform/submit.php" method="POST">
+    <input name="email" placeholder="Email"><br>
+    <input name="name" placeholder="Имя"><br>
+    <input name="phone" placeholder="Телефон"><br>
+    <textarea name="message" placeholder="Сообщение"></textarea><br>
+    <input type="submit" value="Отправить сообщение">
+  </form>
+</body>
+</html>
---
+++ форма пхп/thankyou.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+
+<h1>Сообщение отправлено!</h1>
+
+</body>
+</html>
