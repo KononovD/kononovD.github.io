7a0f7a71664f836c47fe10c6e6dfe54832b1eb9b
KononovD <KononovD@users.noreply.github.com>
2018-09-17 18:04:32 +0300

    1
    1

--- Rock Invest/js/main.js
+++ Rock Invest/js/main.js
@@ -1,2 +1,2 @@
-$("#navToggle").click(function(){$(this).toggleClass("active"),$(".overlay").toggleClass("open")}),$(".overlay").click(function(){$(this).removeClass("open"),$(".navBurger").removeClass("active")});
+$(document).ready(function(){$("#navToggle").click(function(){$(this).toggleClass("active"),$(".overlay").toggleClass("open")}),$(".overlay").click(function(){$(this).removeClass("open"),$(".navBurger").removeClass("active")})});
 //# sourceMappingURL=main.js.map
