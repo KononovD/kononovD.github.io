69d471aae80da6a1f111f03b99b2e7914df92ca5
KononovD <39659265+KononovD@users.noreply.github.com>
2019-02-23 19:18:15 +0200

    1
    1

---
+++ studio gift/js/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".owl-carousel").owlCarousel({loop:!0,margin:20,nav:!0,items:3,dots:!1,smartSpeed:500,navText:['<i class="fas fa-arrow-circle-left"></i>','<i class="fas fa-arrow-circle-right"></i>']})});
+//# sourceMappingURL=main.js.map
